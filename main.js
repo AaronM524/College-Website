34a531d35ee29912af6031cec7c4a816e1298820
AaronM524 <aaronmathew468@gmail.com>
2024-09-16 10:42:36 -0400

    gotta fix the home screen

---
+++ main.js
@@ -0,0 +1,5 @@
+let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+console.log(vw);
+console.log(vh)
